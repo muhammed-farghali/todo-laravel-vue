28c896a83ad3e5ce2191b1ffa676a5941876a244
Muhammed Ahmed <muhammad.farghali14@gmail.com>
2020-06-26 19:56:51 +0000

    create task model and its migration && factory && seeder

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Task;
+use Faker\Generator as Faker;
+
+$factory->define(Task::class, function (Faker $faker) {
+    $rand = rand(0, 1);
+    $startTime = null;
+    $endTime = null;
+    $hours = rand(1, 4);
+
+    if ($rand) {
+        $timestamp = rand( strtotime("Jan 01 2020"), strtotime("Dec 31 2021") );
+        $startTime = date("H:i:s", $timestamp );
+        $endTime = date("H:i:s", ($timestamp + $hours * 60 * 60));
+    }
+
+    return [
+        'name' => $faker->word,
+        'description' => function () use ($faker) {
+            return rand(0, 1) ? rtrim( $faker->sentence( rand( 1, 2 ), '.' ) ) : null; 
+        },
+        'duration' => function () use ($rand, $hours) {
+            if ($rand) {
+                return $hours;
+            }
+            return rand(0, 1) ? rand(1, 5) : null; 
+        },
+        'date' => function () use ($rand) {
+            if ($rand) {
+                $timestamp = rand( strtotime("Jan 01 2020"), strtotime("Dec 31 2021") );
+                $randomDate = date("Y-m-d", $timestamp );
+                return $randomDate;
+            }
+            return null; 
+        },
+        'start_at' => $startTime,
+        'end_at' => $endTime,
+        'user_id' => function () use ($rand) {
+            return $rand ? factory(\App\User::class)->create() : null;
+        }
+    ];
+});
